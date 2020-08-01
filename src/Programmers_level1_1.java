1b225d94353f867bfbf80dcc3964a68d2c7f68b1
Ji-ho <wlgh312@gmail.com>
2020-08-02 00:51:34 +0900

    week1

--- src/Programmers_level1_1.java
+++ src/Programmers_level1_1.java
@@ -2,28 +2,30 @@ import java.util.*;
 
 public class Programmers_level1_1 {
 	public static int solution(int[][] board, int[] moves) {
-        int answer = 0;
-        Stack<Integer> num = new Stack<>();
-        for(int i=0; i<moves.length; i++){//크레인 이동
-            for(int j=board[moves[i]-1].length-1; j>=0; j--){//크레인이 위치해 있는 격자 안의 인형 확인(배열의 마지막부터 확인)
-                if(board[moves[i]-1][j]!=0){//인형이 있다면
-                    if(!num.empty() && num.peek()==board[moves[i]-1][j]) {//스택이 비어있지 않고 마지막 원소가 동일한 인형이라면
-                    	num.pop();//스택의 마지막 원소 삭제
-                        answer+=2;//삭제된 인형의 개수 증가
+		int answer = 0;//사라진 인형 수
+        Stack<Integer> num = new Stack<Integer>();//스택
+        for(Integer m : moves){//크레인 위치
+        	int i=m-1;//크레인 위치를 배열의 인덱스에 맞게 변환
+        	//board=격자의 가로줄로 이루어진 배열
+            for(int j=0; j<board[i].length; j++){//격자의 가로 크기만큼 반복
+            	if(board[j][i]!=0){//크레인이 첫번째 격자의 맨 위의 인형을 만난다면
+                    if(!num.empty() && num.peek()==board[j][i]) {//스택이 비어있지않고 맨 위의 수가 크레인이 잡은 인형과 같다면
+                    	num.pop();//스택의 맨 위의 수 삭제
+                        answer+=2;//사라진 인형 수 2 증가
                     }
-                    else{//스택이 비어있거나 동일한 인형이 아니라면
-                    	num.push(board[moves[i]-1][j]);//스택에 인형 추가
+                    else{//스택이 비어있거나 맨 위의 수가 크레인이 잡은 인형과 다르다면
+                    	num.push(board[j][i]);//스택에 인형 추가
                     }
-                    board[moves[i]-1][j]=0;//격자의 인형 제거
-                    break;//해당 격자에서의 작업 종료
+                    board[j][i]=0;//크레인이 잡은 인형 자리 0으로 변경
+                    break;//다음 크레인 위치로 이동하기 위해 반복문 종료
                 }
              }
         }
         return answer;
     }
 	public static void main(String[] args) {
-		int arr[][]= {{0, 0, 0, 0, 0}, {0, 0, 1, 0, 3}, {0, 2, 5, 0, 1}, {4, 2, 4, 4, 2}, {3, 5, 1, 3, 1}};
-		int moves[] = {1, 5, 3, 5, 1, 2, 1, 4};
+		int arr[][] = {{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}};
+		int moves[] = {1,5,3,5,1,2,1,4};
 		System.out.println(solution(arr, moves));
 	}
 }
---
+++ src/Programmers_level1_2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Programmers_level1_2 {
+	public static String solution(String s) {
+        String answer="";
+        String tmp[] = s.split("");//문자열 나누기
+        Arrays.sort(tmp, Collections.reverseOrder());//문자열 내림차순으로 정렬
+        for(String t : tmp) {//배열을 문자열로 변환
+        	answer+=t;
+        }
+        return answer;
+    }
+	public static void main(String[] args) {
+		String s ="Zbcdefg"; 
+		System.out.println(solution(s));
+	}
+}
